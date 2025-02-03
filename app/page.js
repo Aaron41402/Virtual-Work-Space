3f158ba128b76d918a3e5350906ce91226d7e62d
Aaron41402 <92064345+Aaron41402@users.noreply.github.com>
2025-02-03 15:44:36 -0800

    updated hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1>Get Started</h1>
+      <div>Create a feedback</div>
     </main>
   );
 }
